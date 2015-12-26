e770b0dd785c75cf3caa3b97fd66950b1a420528
Yongzhi Cui <cuiyongzhi@hotmail.fr>
2015-12-26 13:04:17 +0100

    ver1:without class, execution in console only

--- SimpleFactory/src/org/ycui/calc/CalcProgram.java
+++ SimpleFactory/src/org/ycui/calc/CalcProgram.java
@@ -1,13 +1,60 @@
 package org.ycui.calc;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class CalcProgram {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		double result = 0.0d;
+		try{			
+			double num1 = Double.parseDouble(readDataFromConsole("Please input number 1: "));
+			String oper = readDataFromConsole("Please select a operater: (+ - * /)");
+			double num2 = Double.parseDouble(readDataFromConsole("Please input number 2: "));
+			
+			switch(oper){
+			case "+":
+				result = num1 + num2;
+				break;
+			case "-":
+				result = num1 - num2;
+				break;
+			case "*":
+				result = num1 * num2;
+				break;
+			case "/":
+				if(num2!=0){
+					result = num1 / num2;
+				}else{
+					System.out.println("num must not be 0!");
+				}
+				break;
+			
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			System.out.println("wrong input!");
+		}
+	    System.out.println("The result is:  "+ result);
 
 	}
 
+
+private static String readDataFromConsole(String prompt) {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String str = null;
+    try {
+        System.out.print(prompt);
+        str = br.readLine();
+
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+    return str;
+}
 }
