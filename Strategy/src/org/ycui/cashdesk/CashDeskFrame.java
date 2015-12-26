35105a37ff356944ad6b46fe7b1bcc127d45ee08
Yongzhi Cui <cuiyongzhi@hotmail.fr>
2015-12-26 23:47:05 +0100

    Realize a Program 'Cash Desk' with Java GUI

--- Strategy/src/org/ycui/cashdesk/CashDeskFrame.java
+++ Strategy/src/org/ycui/cashdesk/CashDeskFrame.java
@@ -1,8 +1,13 @@
 package org.ycui.cashdesk;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class CashDeskFrame extends Frame {
+import javax.swing.JOptionPane;
+
+@SuppressWarnings("serial")
+public class CashDeskFrame extends Frame{
 	Panel panel = new Panel();
 	Button buttonOK = new Button("  OK  ");
 	Button buttonClear = new Button("Clear");
@@ -37,15 +42,18 @@ public class CashDeskFrame extends Frame {
 		
 		
 		labelTextTotal.setLocation(50, 370);
+		labelCashTotal.setSize(100,23);
 		labelCashTotal.setLocation(120, 370);
 		
 	}
 	public void addComponent(){
 		panel.add(labelPrice);
 		panel.add(textFieldPrice);
+		buttonOK.addActionListener(new ButtonOKListener());
 		panel.add(buttonOK);
 		panel.add(labelQuantity);
 		panel.add(textFieldQuantity);
+		buttonClear.addActionListener(new ButtonClearListener());
 		panel.add(buttonClear);
 		panel.add(textAreaList);
 		panel.add(labelTextTotal);
@@ -54,5 +62,53 @@ public class CashDeskFrame extends Frame {
 		setSize(500, 500);
 		setVisible(true);
 	}
-
+	
+/*	
+	public void clickButtonOK(){ // inner class
+		buttonOK.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent evt){labelCashTotal.setText("OK");}
+		});
+	}
+*/
+	class ButtonOKListener implements ActionListener{ // action on click button "OK"
+		double underTotal;
+		double total;
+		public void actionPerformed(ActionEvent evt){
+			if("_STRING".equals(verifyInput(textFieldPrice.getText()))){ // verify the price
+				JOptionPane.showMessageDialog(null, "Wrong input, please input a corret price!");
+				textFieldPrice.setText("");
+			}
+			else if(!"_INT".equals(verifyInput(textFieldQuantity.getText()))){ // verify the quantity
+				JOptionPane.showMessageDialog(null, "Wrong input, the quantity must be a integer!");
+				textFieldQuantity.setText("");
+			}else{
+				underTotal =  (Double.parseDouble(textFieldPrice.getText()) * Integer.parseInt(textFieldQuantity.getText()));
+				textAreaList.append("Price uni:" + textFieldPrice.getText() + ", Quantity:" + textFieldQuantity.getText() 
+						+ ", under total: " + (Double.parseDouble(textFieldPrice.getText()) * Integer.parseInt(textFieldQuantity.getText())) + "\n");
+				total += underTotal;
+				labelCashTotal.setText(new java.text.DecimalFormat("#0.00").format(total)); // DecimalFormat 2
+			}
+		}
+		
+		private String verifyInput(String str){
+			if (!str.matches("^[0-9]+([.]{0,1}[0-9]+){0,1}$")){
+		        return "_STRING";   
+		    }else if (str.matches("^[0-9]+$")){
+		        return "_INT";
+		    }else {
+		        return "_DOUBLE";
+		    }
+		} 
+	}
+	
+	class ButtonClearListener implements ActionListener{ // action on click button "clear"
+		public void actionPerformed(ActionEvent evt){
+			textFieldPrice.setText("");
+			textFieldQuantity.setText("");
+			textAreaList.setText("");
+			labelCashTotal.setText("");
+		}
+	}
 }
+
+
--- Strategy/src/org/ycui/cashdesk/CashDeskProgram.java
+++ Strategy/src/org/ycui/cashdesk/CashDeskProgram.java
@@ -1,13 +1,11 @@
 package org.ycui.cashdesk;
 
-import java.awt.*;
-
 public class CashDeskProgram {
 
 	public static void main(String[] args) {
 	    CashDeskFrame cdf = new CashDeskFrame("Cash Desk");    
 	    cdf.addComponent();
-	    cdf.formatComponent();	    
+	    cdf.formatComponent();	
 	}
 
 }
